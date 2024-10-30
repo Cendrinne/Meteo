c4317988bb0d55bea8d60c66a9149ca418d9a406
Cendrinne <nancy13lg@gmail.com>
2024-10-30 17:23:01 -0400

    removed "" in metric in apiUrl in js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function refreshWeather(response) {
 
 function searchCity(city) {
 let apiKey = "73f90ofbe4423241037fca0b58a62fdt";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units="metric"`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(refreshWeather);
 }
 
