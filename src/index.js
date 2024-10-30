44d3d53fe563b600afc9f89c467b1828afc6be0e
Cendrinne <nancy13lg@gmail.com>
2024-10-30 17:36:12 -0400

    saved html & js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = response.data.temperature.current;
-  }
-
-
-
-function searchCity(city) {
-let apiKey = "73f90ofbe4423241037fca0b58a62fdt";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(refreshWeather);
 }
 
-
-
+function searchCity(city) {
+  let apiKey = "73f90ofbe4423241037fca0b58a62fdt";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 
 function handleSearchSubmit(event) {
   event.preventDefault();
