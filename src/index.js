750af2da9f13a76f011a36bb564f3fa045e3d9af
Cendrinne <nancy13lg@gmail.com>
2024-10-30 18:11:47 -0400

    added innerHTML to descriptionElement

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function refreshWeather(response) {
   let descriptionElement = document.querySelector("#description")
 
   cityElement.innerHTML = response.data.city;
-  descriptionElement = response.data.condition.description;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
 
   console.log(response.data.condition.description);
