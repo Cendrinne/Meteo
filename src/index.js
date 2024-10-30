80661ecdb3a58053b9fd61b17ea53ba6a5f82e80
Cendrinne <nancy13lg@gmail.com>
2024-10-30 18:17:16 -0400

    added new humidityElement to js and ID in html

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description")
+  let humidityElement = document.querySelector("#humidity");
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
